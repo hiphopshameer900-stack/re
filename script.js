71202cd45679e20b90ca61a0f1f7ebd525ca9f75
hiphopshameer900-stack <hiphopshameer900@gmail.com>
2026-06-28 12:47:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// ===============================
+// PROJECT AMMU ❤️
+// Scene 1
+// ===============================
+
+console.log("Project Ammu Loaded ❤️");
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+    alert("❤️ Welcome to My Heart ❤️");
+});
